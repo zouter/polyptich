457d6ee766ffd97f10abedc72043c6d925fd6d61
Wouter.Saelens <wouter.saelens@gmail.com>
2025-01-09 12:09:07 +0100

    version v0.0.16

--- src/polyptich/grid/grid.py
+++ src/polyptich/grid/grid.py
@@ -272,7 +272,6 @@ class Grid(Element):
 
         for row, row_elements in enumerate(self.elements):
             for col, el in enumerate(row_elements):
-                print(el.height)
                 if el is not None:
                     el.align()
                     if el.width is None:
